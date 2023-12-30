bd2aac9e426730bb452679743bfdaf73f91b7e9f
timedisonn <12722141-timedisonn@users.noreply.replit.com>
2023-12-30 12:28:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,73 @@
+var timeLeft = document.getElementById('sessionTime').value * 60;
+var breakTime = document.getElementById('breakTime').value * 60;
+var timerId;
+var isBreak = false;
+var sessionCount = 0;
+
+function startTimer() {
+  stopTimer(); // Stop any running timer
+  timerId = setInterval(function() {
+    timeLeft--;
+    var minutes = Math.floor(timeLeft / 60);
+    var seconds = timeLeft % 60;
+    document.getElementById('timer').textContent = minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+    if (timeLeft <= 0) {
+      isBreak = !isBreak;
+      if (isBreak) {
+        timeLeft = breakTime;
+        if (++sessionCount >= 4) {
+          timeLeft = 30 * 60; // Long break
+          showAlert('Time for your long break');
+        } else {
+          showAlert('Time for your break');
+        }
+      } else {
+        timeLeft = document.getElementById('sessionTime').value * 60;
+      }
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timerId);
+}
+
+function resetTimer() {
+  stopTimer();
+  isBreak = false;
+  sessionCount = 0;
+  timeLeft = document.getElementById('sessionTime').value * 60;
+  document.getElementById('timer').textContent = timeLeft / 60 + ':00';
+}
+
+function applyChanges() {
+  stopTimer();
+  timeLeft = document.getElementById('sessionTime').value * 60;
+  breakTime = document.getElementById('breakTime').value * 60;
+  document.getElementById('timer').textContent = timeLeft / 60 + ':00';
+}
+
+function showAlert(message) {
+  var alertElement = document.getElementById('alert');
+  alertElement.textContent = message;
+  alertElement.style.display = 'block';
+  setTimeout(function() {
+    alertElement.style.display = 'none';
+  }, 5000); // Hide after 5 seconds
+}
+
+// Then, in your startTimer function:
+if (timeLeft <= 0) {
+  isBreak = !isBreak;
+  if (isBreak) {
+    timeLeft = breakTime;
+    if (++sessionCount >= 4) {
+      timeLeft = 30 * 60; // Long break
+      showAlert('Time for your long break');
+    } else {
+      showAlert('Time for your break');
+    }
+  } else {
+    timeLeft = document.getElementById('sessionTime').value * 60;
+  }
+}
